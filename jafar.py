26fe9730952fb9f72f1e183fcf9234a5b3f9d070
LaCapitainerie <hugo.antreassian@gmail.com>
2024-04-25 22:25:51 +0200

    Fixed Code For PyLint

--- jafar.py
+++ jafar.py
@@ -1,13 +1,28 @@
-from collections import Counter;
-from itertools import permutations;
-import pandas as pd;
+"""
+Importation des modules nécessaires
+"""
+from collections import Counter
+from itertools import permutations
+import pandas as pd
 
-# Fonction principale
 def predire(csv:str, col:list[str], *, prediction:list[int]) -> int:
+    """
+    Fonction principale de prédiction
+    :param csv: Fichier CSV
+    :param col: Liste des colonnes à lire
+    :param prediction: Liste des nombres à trouver
+    :return: 0
+    """
     return result(*lecture_csv(csv=csv, col=col), prediction=prediction)
 
 # Fonctions secondaires
 def lecture_csv(csv:str, col:list[str]) -> tuple[Counter, int, int, int]:
+    """
+    Fonction de lecture du fichier CSV
+    :param csv: Fichier CSV
+    :param col: Liste des colonnes à lire
+    :return: Counter, int, int, int
+    """
     if not csv.endswith(".csv"):
         raise ValueError("Le fichier doit être un fichier CSV")
     if csv == "":
@@ -25,11 +40,26 @@ def lecture_csv(csv:str, col:list[str]) -> tuple[Counter, int, int, int]:
 
 # Fonctions de calcul
 def moy_a_16(d:Counter, t:int, size:int) -> int:
+    """
+    Fonction de calcul de la moyenne à 16
+    :param d: Counter
+    :param t: int
+    :param size: int
+    :return: int
+    """
     # Formule avec Carré plutôt que Absolue
-    return sum([(r:=val*size - t)*r for val in d.values()])
+    return sum((r:=val*size - t)*r for val in d.values())
 
 # Fonction de calcul de l'écart type
 def ecart_type(d:Counter, t:int, size:int, col_len:int) -> dict[tuple[int, ...], int]:
+    """
+    Fonction de calcul de l'écart type
+    :param d: Counter
+    :param t: int
+    :param size: int
+    :param col_len: int
+    :return: dict[tuple[int, ...], int]
+    """
     # Dictionnaire des combinaisons et leur pourcentage
     percent:dict[int, int] = {}
 
@@ -39,21 +69,30 @@ def ecart_type(d:Counter, t:int, size:int, col_len:int) -> dict[tuple[int, ...],
     # Calcul des combinaisons
     for each in permutations(d, r=col_len):
         print(each, end="\r", )
-        if (eachTuple := tuple(sorted(each))) not in values_already_seen:
+        if (each_tuple := tuple(sorted(each))) not in values_already_seen:
             values_already_seen.add(each)
-            percent[eachTuple] = moy_a_16(d + Counter(eachTuple), t+1, size)
+            percent[each_tuple] = moy_a_16(d + Counter(each_tuple), t+1, size)
 
     return percent
 
 # Fonction de résultat
 def result(d:Counter[int, int], t:int, size:int, tirage:int, *, prediction:list[int]) -> int:
+    """
+    Fonction de résultat
+    :param d: Counter
+    :param t: int
+    :param size: int
+    :param tirage: int
+    :param prediction: list[int]
+    :return: int
+    """
 
     # Retourne la liste triée des écarts types
     s = sorted(ecart_type(d, t, size, tirage).items(), key=lambda x: x[1])
     prediction:tuple[int] = tuple(sorted(prediction))
 
     # Ecriture dans un fichier
-    with open("result.txt", "w") as f:
+    with open("result.txt", "w", encoding="utf8") as f:
         for each, _ in s:
             f.write(f"{each} : {'X' if each == prediction else ''}\n")
 
