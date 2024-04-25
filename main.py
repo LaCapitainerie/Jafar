5d8736f719c24e1bcc5d8b5c0ae6d3fccee4b4c1
LaCapitainerie <hugo.antreassian@gmail.com>
2024-04-25 22:20:55 +0200

    Fixed Code For PyLint

--- main.py
+++ main.py
@@ -2,9 +2,14 @@ from jafar import predire;
 
 # Liste des nombres à trouver
 prediction:list[int] = [3]
-chances:list[str]   = ["numero_chance"]
-normal:list[str]   = ["boule_1", "boule_2", "boule_3", "boule_4", "boule_5"]
-csvFile:str          = "loto_201911.csv"
+chances:list[str] = ["numero_chance"]
+
+# Liste des boules normales
+prediction:list[int] = [8, 28, 40, 41, 42]
+normal:list[str] = ["boule_1", "boule_2", "boule_3", "boule_4", "boule_5"]
+
+# Fichier CSV
+csvFile:str = "loto_201911.csv"
 
 # Lecture et calcul des resultats
 predire(csv=csvFile, col=normal, prediction=prediction)
